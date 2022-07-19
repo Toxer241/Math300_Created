41aa9a42c76515a43cf14a944a7ef51ae5818392
Toxer241 <51790458+Toxer241@users.noreply.github.com>
2022-07-19 00:32:36 -0700

    Delete Problem5.py

--- Problem5.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-# import sympy and numpy
-import sympy as sp
-
-# calculate the derivative
-x = sp.symbols('x')
-y = x**3 - sp.cos((sp.pi*x)**2)/(2*x**2 + 1) + 11*x**2/3 - 1
-print("f\'(x) = ", sp.diff(y, x))
